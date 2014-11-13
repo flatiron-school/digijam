e78a558e7602ba8fd7410f78417b0b4e023bd45d
zdrossman <zachary.drossman@gmail.com>
2014-11-13 17:25:32 -0500

    Added gitignore and gitattributes

---
+++ Digijam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digijam
+//
+//  Created by Zachary Drossman on 11/13/14.
+//  Copyright (c) 2014 Zachary Drossman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
