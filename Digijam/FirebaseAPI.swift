a5025b169d427671b25e4e7a13401a264895314f
Joe Burgess <joe@flatironschool.com>
2014-11-24 13:41:01 -0500

    Adding firebase -> user method

--- Digijam/FirebaseAPI.swift
+++ Digijam/FirebaseAPI.swift
@@ -26,4 +26,27 @@ class FirebaseAPI: NSObject {
             println(JSON)
         }
     }
+
+    class func post (user : User, completionBlock: ((userID : String, success : Bool) -> Void)?)
+    {
+        let requestURL = NSURL(string: "\(PrivateKeys.FIREBASEURL)/users.json")
+        let request = NSMutableURLRequest(URL: requestURL!)
+        request.HTTPMethod = "POST"
+
+        let userDictionary = ["firstName": user.firstName, "lastName": user.lastName, "courseName":user.courseName, "githubID":user.githubID]
+
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(userDictionary, options: nil, error: nil)
+
+        Alamofire.request(request).responseJSON { (httpRequest, httpResponse, JSON, error) -> Void in
+            if let jsonresponse : [String:String] = JSON as? [String:String]
+            {
+                if let completion = completionBlock
+                {
+                    completion(userID: jsonresponse["name"]!, success:true)
+                }
+            }
+        }
+
+
+    }
 }
