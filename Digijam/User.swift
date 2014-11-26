062ef5e44795e60007f778cd9001369c3fd38d9e
Joe Burgess <joe@flatironschool.com>
2014-11-26 08:52:16 -0500

    Adding Firebase ID to user

--- Digijam/User.swift
+++ Digijam/User.swift
@@ -2,7 +2,7 @@
 //  User.swift
 //  Digijam
 //
-//  Created by Joe Burgess on 11/24/14.
+//  Created by Joe Burgess on 11/26/14.
 //  Copyright (c) 2014 Zachary Drossman. All rights reserved.
 //
 
@@ -11,9 +11,10 @@ import CoreData
 
 class User: NSManagedObject {
 
-    @NSManaged var githubID: String
-    @NSManaged var firstName: String
-    @NSManaged var lastName: String
     @NSManaged var courseName: String
+    @NSManaged var firebaseID: String
+    @NSManaged var firstName: String
+    @NSManaged var githubID: String
+    @NSManaged var lastName: String
 
 }
